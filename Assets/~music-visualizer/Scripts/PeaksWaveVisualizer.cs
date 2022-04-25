48d3956b4374262eb6cf94473c11390d4b4cf90d
Artur Dedyulya <aeternum-vale@ya.ru>
2022-04-25 21:42:55 +0300

    add bloom

--- Assets/~music-visualizer/Scripts/PeaksWaveVisualizer.cs
+++ Assets/~music-visualizer/Scripts/PeaksWaveVisualizer.cs
@@ -5,8 +5,10 @@ using NAudioWaveFormRenderer;
 using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class MainController : MonoBehaviour
+public class PeaksWaveVisualizer : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private string _filePath;
@@ -22,8 +24,14 @@ public class MainController : MonoBehaviour
     [Space] [SerializeField] private RectTransform _peaksContainer;
     [SerializeField] private TMP_Text _timeText;
 
+    [Space] [SerializeField] private float _waveMaxBloom = 0.2f;
+    [SerializeField] private float _waveBloomInterpolation = 1f;
+    [SerializeField] private float _waveBloomPower = 1f;
+    [SerializeField] private EPeakProviderType _waveBloomPeakProvider = EPeakProviderType.Max;
+    [SerializeField] private VolumeProfile _ppVolumeProfile;
 
     [Space] [SerializeField] private bool _testMode = false;
+
 
     private MaxMinPeak[] _peakInstances;
     private float[] _readBuffer;
@@ -32,9 +40,12 @@ public class MainController : MonoBehaviour
     private float _currentTime;
     private TimeSpan _audioDuration;
     private List<PeakInfo> _peakInfos;
+    private List<PeakInfo> _waveBgPeakInfos;
     private PeakInfo[] _targetPeakHeights;
     private PeakInfo _defaultPeakInfo;
     private int _lastCenterPeakInfoIndex;
+    private Bloom _ppBloom;
+
 
     private void Awake()
     {
@@ -42,6 +53,31 @@ public class MainController : MonoBehaviour
         _targetPeakHeights = new PeakInfo[_peakInstances.Length];
         _maxPeakHeight = _peaksContainer.rect.height / 2;
         _defaultPeakInfo = new PeakInfo(-_peakMinHeight / _maxPeakHeight, _peakMinHeight / _maxPeakHeight);
+
+        _ppVolumeProfile.TryGet(out _ppBloom);
+    }
+
+    private void Start()
+    {
+        Play();
+    }
+
+    [Button]
+    private void Play()
+    {
+        RetrieveAudioInfo(GetPeakProvider(_peakProviderType), out _peakInfos, out _audioDuration);
+        if (_peakProviderType != _waveBloomPeakProvider)
+            RetrieveAudioInfo(GetPeakProvider(_waveBloomPeakProvider), out _waveBgPeakInfos, out _);
+        else
+            _waveBgPeakInfos = _peakInfos;
+
+        _audioSource.clip.LoadAudioData();
+
+        _currentTime = 0;
+        _timerIsRunning = true;
+        _lastCenterPeakInfoIndex = -1;
+
+        _audioSource.Play();
     }
 
     private void RetrieveAudioInfo(IPeakProvider peakProvider, out List<PeakInfo> peakInfos, out TimeSpan duration)
@@ -64,9 +100,9 @@ public class MainController : MonoBehaviour
         }
     }
 
-    private IPeakProvider GetPeakProvider()
+    private IPeakProvider GetPeakProvider(EPeakProviderType type)
     {
-        switch (_peakProviderType)
+        switch (type)
         {
             case EPeakProviderType.Average:
                 return new AveragePeakProvider(4);
@@ -83,18 +119,6 @@ public class MainController : MonoBehaviour
         }
     }
 
-    [Button]
-    private void Play()
-    {
-        RetrieveAudioInfo(GetPeakProvider(), out _peakInfos, out _audioDuration);
-
-        _currentTime = 0;
-        _timerIsRunning = true;
-        _lastCenterPeakInfoIndex = -1;
-
-        
-        _audioSource.Play();
-    }
 
     private void Update()
     {
@@ -157,6 +181,18 @@ public class MainController : MonoBehaviour
             peakInstance.MinPeak.rectTransform.sizeDelta =
                 new Vector2(sizeDelta.x, Mathf.Lerp(sizeDelta.y, targetHeights.Min, finalPeakInterpolation));
         }
+        
+        //TODO make it fps-independent
+        float newWaveBgAlpha =
+            Mathf.Lerp(
+                _ppBloom.intensity.value,
+                Mathf.Pow(
+                    Mathf.Clamp(_waveBgPeakInfos[centerPeakInfoIndex].Max, 0f, 1f),
+                    _waveBloomPower) *
+                _waveMaxBloom,
+                _waveBloomInterpolation);
+
+        _ppBloom.intensity.value = newWaveBgAlpha;
     }
 
     private void DisplayTime(float timeToDisplay)
